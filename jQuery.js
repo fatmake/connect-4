751da173aea44e8f9c2b555a1218a1a6aa659511
fatmake <fatmakelantan@outlook.com>
2019-12-05 21:26:10 +0300

    V2

--- jQuery.js
+++ jQuery.js
@@ -3,12 +3,11 @@ var turn = 1;
 
 $('document').ready(function(){
 
-	var redArray = [];
-	var yellowarray = [];
 
 	var j1 = 4; //rows
+	
 
-	$('.col').click(function(){
+	$('#col1').click(function(){
 
 		var i = 1; //columns
 
@@ -17,6 +16,7 @@ $('document').ready(function(){
 						
 			if( turn % 2 == 0){ 
 				$("#coin"+j1+"-"+i).css('background-color','yellow');
+				$("#coin"+j1+"-"+i).addClass("player2");
 				$('#p1').addClass('t'); //highlight the current players name
 				$('#p2').removeClass('t');
 							//yellowArray.push(j1+''+i);
@@ -24,26 +24,27 @@ $('document').ready(function(){
 				}
 				else{
 					$("#coin"+j1+"-"+i).css('background-color','red');
+					$("#coin"+j1+"-"+i).addClass("player1");
 					$('#p2').addClass('t');//highlight the current players name
 					$('#p1').removeClass('t');
-					$("#coin"+j1+"-"+i).slideDown('slow');
-					redArray.push(j1+'-'+i);
 					turn++;
 				}
+				checkWinner();
 
 			}
-					console.log(redArray);//output: ["4-1", "3-1", "2-1", "1-1"]
+					//console.log(redArray);//output: ["4-1", "3-1", "2-1", "1-1"]
+		
 		j1--;//next row
-		i++;
-		if (redArray == ["4-1", "3-1", "2-1", "1-1"]){//it doesn't go inside this if
-			alert('Player 1 WINS !!');
-		}
+
+		
 
 });
 
-	/*var j2 = 4; //rows
 
-	$('.col2').click(function(){
+
+	var j2 = 4; //rows
+
+	$('#col2').click(function(){
 
 			var i = 2; //columns
 
@@ -51,27 +52,32 @@ $('document').ready(function(){
 						
 						if( turn % 2 == 0){ 
 							$("#coin"+j2+"-"+i).css('background-color','yellow');
+							$("#coin"+j2+"-"+i).addClass("player2");
 							$('#p1').addClass('t');
 							$('#p2').removeClass('t');
 							turn++;
 						}
 						else{
 							$("#coin"+j2+"-"+i).css('background-color','red');
+							$("#coin"+j2+"-"+i).addClass("player1");
 							$('#p2').addClass('t');
 							$('#p1').removeClass('t');
 							turn++;
 
 						}
-
+						
+						checkWinner();
 					}
-			j2--;	
+			
+			j2--;
+				
 
 
 	});
 
 	var j3 = 4; //rows
 
-	$('.col3').click(function(){
+	$('#col3').click(function(){
 
 			var i = 3; //columns
 
@@ -79,27 +85,32 @@ $('document').ready(function(){
 						
 						if( turn % 2 == 0){ 
 							$("#coin"+j3+"-"+i).css('background-color','yellow');
+							$("#coin"+j3+"-"+i).addClass("player2");
 							$('#p1').addClass('t');
 							$('#p2').removeClass('t');
 							turn++;
 						}
 						else{
 							$("#coin"+j3+"-"+i).css('background-color','red');
+							$("#coin"+j3+"-"+i).addClass("player1");
 							$('#p2').addClass('t');
 							$('#p1').removeClass('t');
 							turn++;
 
 						}
+						checkWinner();
 
 					}
-			j3--;	
+			
+			j3--;
+				
 
 
 	});
 
 	var j4 = 4; //rows
 
-	$('.col4').click(function(){
+	$('#col4').click(function(){
 
 			var i = 4; //columns
 
@@ -107,40 +118,142 @@ $('document').ready(function(){
 						
 						if( turn % 2 == 0){ 
 							$("#coin"+j4+"-"+i).css('background-color','yellow');
+							$("#coin"+j4+"-"+i).addClass("player2");
 							$('#p1').addClass('t');
 							$('#p2').removeClass('t');
 							turn++;
 						}
 						else{
 							$("#coin"+j4+"-"+i).css('background-color','red');
+							$("#coin"+j4+"-"+i).addClass("player1");
 							$('#p2').addClass('t');
 							$('#p1').removeClass('t');
 							turn++;
 
 						}
+						checkWinner();
 
 					}
-			j4--;	
+					
+			j4--;
+				
 
 
-	});*/
+	});
+
+	function checkWinner(){
+		var yellowCounter;
+		var redCounter;
+		/////columns//////
+		
+		for (var i = 1;i <= 4;i++){
+		//alert($("#coin"+4+"-"+i).hasClass('player2'));
+			yellowCounter = 0;
+			redCounter = 0;
+		
+			for (var j = 1;j <= 4;j++){
+				
+				if($("#coin"+j+"-"+i).hasClass('player2')){
+					yellowCounter++;
+					console.log(yellowCounter+' yellow');
+
+				}
+				if($("#coin"+j+"-"+i).hasClass('player1')){
+					redCounter++;
+					console.log(redCounter+' red');
+				}
+
+			}
+			
+
+		}
+			if(yellowCounter == 4 ){
+				alert('Player 2 WINS (col)!!');
+			}
+			if(redCounter == 4 ){
+				alert('Player 1 WINS (col)!!');
+			}
+
+		//////rows/////
+		    
+		for (var j = 1;j <= 4;j++){
+		//alert($("#coin"+1+"-"+i).css('background-color'));
+		yellowCounter = 0;
+			redCounter = 0;
+			
+			for (var i = 1;i <= 4;i++){
+				
+				if($("#coin"+j+"-"+i).hasClass('player2')){
+					yellowCounter++;
+				}
+				if($("#coin"+j+"-"+i).hasClass('player1')){
+					redCounter++;
+				}
+
+			}	
+
+		}
+			if(yellowCounter == 4 ){
+				alert('Player 2 WINS (rows)!!');
+			}
+			else if(redCounter == 4 ){
+				alert('Player 1 WINS (rows)!!');
+		}
+
+
+		///////tie/////
+		var flag;
+		for (var i = 1; i<=4 ;i++){
+			for (var j = 4; j >= 1; j--){
+				//set all the cells to the original color=white
+				if($("#coin"+j+"-"+i).css('background-color') !== 'rgb(255, 255, 255)'){
+					flag=true;
+				}
+				else{
+					flag=false;
+				}
+
+			}
+		}
+		if(flag == true){
+			alert("It's a tie");
+		}
+
+		////diagonal (left-right)////
+		/*var col = 1;
+		var row = 4;
+		while(i <= 4){
+		if($("#coin"+row+"-"+col).attr('background-color') == 'rgb(255, 0, 0)'){
+					flag=true;
+				}
+				else{
+					flag=false;
+				}
+				row--;
+
+		}
+		col++;
+
+		if(flag == true){
+			alert("Player 1 WINS!!");
+		}*/
+	}
 
 
 	
 	$('button').click(function(){// to restart the game
-		//$(this).removeClass('.col1');
+	
+		{location.reload(true);}
 
-		for (var i = 1; i<=4 ;i++){
+		/*for (var i = 1; i<=4 ;i++){
 			for (var j = 4; j >= 1; j--){
 				//set all the cells to the original color=white
 				$("#coin"+j+"-"+i).css('background-color','rgb(255, 255, 255)');
 
 			}
-		}
+		}*/
 
 	});
-
-	//$('button').off('click');
 
 });
 
